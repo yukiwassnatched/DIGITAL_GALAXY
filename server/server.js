437a1ca49dbccaaac0a1d5048c6eb44f91ce5f37
Yuktha <yukthasingh@hotmail.com>
2026-02-03 20:00:50 -0500

    fixed server.js to load firebase key from environment variables instead of a file when reading on render

--- server/server.js
+++ server/server.js
@@ -14,9 +14,11 @@ const cookieParser = require("cookie-parser");
 const fs = require("fs");
 
 const admin = require("firebase-admin");
-const serviceAccount = JSON.parse(
-  fs.readFileSync(".env/firebaseKey.json", "utf8")
-); // Firebase service account key
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
